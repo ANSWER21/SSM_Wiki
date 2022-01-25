a0a3ac9020f09be00bdfc7a723b1078c34fafb41
ANSWER21 <40889529+ANSWER21@users.noreply.github.com>
2022-01-25 15:10:33 +0800

    启动类位置重构：需要添加@ComponentScan注解 添加helloworld接口

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,4 +1,4 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 
 
 
@@ -10,9 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("com.jiawa")
 @SpringBootApplication
 public class WikiApplication {
 
