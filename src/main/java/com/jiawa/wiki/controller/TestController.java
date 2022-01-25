728edcdbaf976988e60d473cadd3e80aed2d1ef1
ANSWER21 <40889529+ANSWER21@users.noreply.github.com>
2022-01-25 16:22:42 +0800

    配置文件介绍+@value注解的配置文件方式

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello:AAA}")
+    private  String testHello;
     //localhost:8880//hello/
     @RequestMapping("/hello")
     public String hello(){
-      return "hello world";
+      return "hello world" +testHello;
     };
 }
